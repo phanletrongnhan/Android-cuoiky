b5432b5220054b954d4113b774d693ff2be2b501
phanletrongnhan <58338443+phanletrongnhan@users.noreply.github.com>
2020-01-06 01:59:49 +0700

    Reload meo li thuyet va thuc hanh

--- BangLaiXe/app/src/main/java/com/example/banglaixe/Meo_Thuc_Hanh.java
+++ BangLaiXe/app/src/main/java/com/example/banglaixe/Meo_Thuc_Hanh.java
@@ -1,4 +1,12 @@
 package com.example.banglaixe;
 
 public class Meo_Thuc_Hanh {
+    public String TenMeoTH;
+    public String NoiDungMeoTH;
+
+    public Meo_Thuc_Hanh(String tenMeoTH, String noiDungMeoTH) {
+        TenMeoTH = tenMeoTH;
+        NoiDungMeoTH = noiDungMeoTH;
+    }
 }
+
