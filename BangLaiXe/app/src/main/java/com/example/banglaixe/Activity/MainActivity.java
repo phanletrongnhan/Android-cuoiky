6a5e135f740d7433254bcef50dca1ec43a07dcfb
phanletrongnhan <58338443+phanletrongnhan@users.noreply.github.com>
2020-01-07 03:22:35 +0700

    Update MainActivity.java

--- BangLaiXe/app/src/main/java/com/example/banglaixe/Activity/MainActivity.java
+++ BangLaiXe/app/src/main/java/com/example/banglaixe/Activity/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.banglaixe.R;
 
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void M_click(View view) {
         Intent intent = new Intent(MainActivity.this, Activity_Meo.class);
+        Toast.makeText(getApplicationContext(),"hsdaasd",Toast.LENGTH_SHORT);
         startActivity(intent);
     }
 }
